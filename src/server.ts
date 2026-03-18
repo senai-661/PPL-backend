8c4666a05e6835a910abe70fb33db3e9fe009df4
PEDRO HENRIQUE SANTOS ROQUE <pedro.roque2@portalsesisp.org.br>
2026-03-18 11:31:44 -0300

    Feat: Centralizando Handle de Erros

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import { router } from "./routes.js";
+import { errorMiddleware } from "./middlewares/ErrorMiddleware.js";
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(router);
+server.use(errorMiddleware); // ⚠️ always last
 export { server };
