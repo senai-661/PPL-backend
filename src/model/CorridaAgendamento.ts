e2ad9176751d616628de19fa0423e4f76ef793ce
LARISSA DOS SANTOS <larissa.santos30@portalsesisp.org.br>
2026-04-24 14:30:23 -0300

    feat: cria DTO de corrida agendada
    Adiciona interface CorridaAgendamentoDTO para padronizar requisições de agendamento de corrida.

---
+++ src/model/CorridaAgendamento.ts
@@ -0,0 +1,28 @@
+import { CorridaAgendamentoDTO } from "../interface/CorridaAgendamentoDTO.js";
+
+export class CorridaModel {
+  constructor(private db: any) {}
+
+
+  public async criarAgendamento(data: CorridaAgendamentoDTO) {
+    const query = `
+      INSERT INTO corridas_agendadas 
+      (id_passageiro, origem_corrida, destino_corrida, tipo_corrida, data_agendada, status_agendamento, preco)
+      VALUES ($1,$2,$3,$4,$5,$6,$7)
+      RETURNING *;
+    `;
+
+    const values = [
+      data.idPassageiro,
+      data.origemCorrida,
+      data.destinoCorrida,
+      data.tipoCorrida,
+      data.dataAgendada,
+      data.statusAgendamento,
+      data.preco,
+    ];
+
+    const result = await this.db.pool.query(query, values);
+    return result.rows[0];
+  }
+}
